e532b2b19fcf7a74bd8f4101e7e659b4727fab8e
angelakim <akim173@calpoly.edu>
2024-05-31 15:15:00 -0700

    Added react routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from "react";
-import "./App.css";
+// App.jsx
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Papa from "papaparse";
+import HeroSection from "./components/HeroSection";
 import FinanceDashboard from "./components/FinanceDashboard";
 
-function App() {
+const App = () => {
+  const [fileData, setFileData] = useState([]);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    Papa.parse(file, {
+      header: true,
+      complete: (results) => {
+        const parsedData = results.data;
+        setFileData(parsedData);
+      },
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <FinanceDashboard />
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-white text-black">
+        <Routes>
+          <Route
+            path="/"
+            element={<HeroSection onFileUpload={handleFileUpload} />}
+          />
+          <Route
+            path="/dashboard"
+            element={<FinanceDashboard data={fileData} />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
